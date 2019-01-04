ebbe9e8b5395a47b4ab62af37f3f75b37d95b838
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2019-01-04 23:28:04 +0100

    dodanie zfunkcji

--- main.c
+++ main.c
@@ -29,33 +29,33 @@ typedef struct _wiersz_node {
 void
 appendWiersz(WierszNode **head_ref, double m_barionowa, double m_grawitacyjna, double m_pedu, double R, double omega,
              double P, double Ro_B, double N_B, double entalpia, double r_eq, double s_r, double s_e) {
-    WierszNode *new_wiersz = (WierszNode *) malloc(sizeof(WierszNode));
+    WierszNode *wyliczony = (WierszNode *) malloc(sizeof(WierszNode));
     WierszNode *current_node = *head_ref;
 
-    new_wiersz->next = NULL;
-    new_wiersz->m_barionowa = m_barionowa;
-    new_wiersz->m_grawitacyjna = m_grawitacyjna;
-    new_wiersz->m_pedu = m_pedu;
-    new_wiersz->R = R;
-    new_wiersz->omega = omega;
-    new_wiersz->P = P;
-    new_wiersz->Ro_B = Ro_B;
-    new_wiersz->N_B = N_B;
-    new_wiersz->entalpia = entalpia;
-    new_wiersz->r_eq = r_eq;
-    new_wiersz->s_r = s_r;
-    new_wiersz->s_e = s_e;
+    wyliczony->next = NULL;
+    wyliczony->m_barionowa = m_barionowa;
+    wyliczony->m_grawitacyjna = m_grawitacyjna;
+    wyliczony->m_pedu = m_pedu;
+    wyliczony->R = R;
+    wyliczony->omega = omega;
+    wyliczony->P = P;
+    wyliczony->Ro_B = Ro_B;
+    wyliczony->N_B = N_B;
+    wyliczony->entalpia = entalpia;
+    wyliczony->r_eq = r_eq;
+    wyliczony->s_r = s_r;
+    wyliczony->s_e = s_e;
 
 
     if (*head_ref == NULL) {
-        *head_ref = new_wiersz;
+        *head_ref = wyliczony;
         return;
     }
 
     while (current_node->next != NULL)
         current_node = current_node->next;
 
-    current_node->next = new_wiersz;
+    current_node->next = wyliczony;
 }
 
 WierszNode *findByMBar(WierszNode **head_ref, double m_bar) {
@@ -86,6 +86,7 @@ void destroyList(WierszNode **head_ref) {
 typedef struct _para_wierszy {
     WierszNode *mniejszy;
     WierszNode *wiekszy;
+    double m_bar_szukana;
 } ParaWierszy;
 
 ParaWierszy *znajdzNajblizszaPare(WierszNode **head_ref, double m_bar_szukana) {
@@ -123,8 +124,45 @@ ParaWierszy *znajdzNajblizszaPare(WierszNode **head_ref, double m_bar_szukana) {
     ParaWierszy *paraWierszy = malloc(sizeof(ParaWierszy));
     paraWierszy->mniejszy = mniejsza;
     paraWierszy->wiekszy = wieksza;
+    paraWierszy->m_bar_szukana = m_bar_szukana;
 
     return paraWierszy;
+}
+
+double liniowaZaleznosc(double min_y, double max_y, double min_x, double max_x) {
+    double a, a1, a2, b, b1;
+    a1 = (max_y - min_y);
+
+    // wyliczanie masy grawitacyjnej
+    a2 = (max_x - min_x);
+    a = a1 / a2;
+
+    b1 = ((min_y * max_x) - (max_y * min_x));
+    b = b1 / a2;
+
+    return (min_y - b) / a;
+}
+
+WierszNode *wyliczWierszLiniowo(ParaWierszy *paraWierszy) {
+    WierszNode *wyliczony = malloc(sizeof(WierszNode));
+    WierszNode *mniejszy = paraWierszy->mniejszy;
+    WierszNode *wiekszy = paraWierszy->wiekszy;
+
+    wyliczony->next = NULL;
+    wyliczony->m_barionowa = paraWierszy->m_bar_szukana;
+    wyliczony->m_grawitacyjna = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->m_grawitacyjna, wiekszy->m_grawitacyjna);
+    wyliczony->m_pedu = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->m_pedu, wiekszy->m_pedu);
+    wyliczony->R = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->R, wiekszy->R);
+    wyliczony->omega = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->omega, wiekszy->omega);
+    wyliczony->P = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->P, wiekszy->P);
+    wyliczony->Ro_B = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->Ro_B, wiekszy->Ro_B);
+    wyliczony->N_B = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->N_B, wiekszy->N_B);
+    wyliczony->entalpia = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->entalpia, wiekszy->entalpia);
+    wyliczony->r_eq = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->r_eq, wiekszy->r_eq);
+    wyliczony->s_r = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->s_r, wiekszy->s_r);
+    wyliczony->s_e = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->s_e, wiekszy->s_e);
+
+    return wyliczony;
 }
 
 
@@ -140,12 +178,12 @@ int main(int argc, char **argv) {
 
     WierszNode *lista_wierszy = NULL;
 
-    appendWiersz(&lista_wierszy, 1.1, 67, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-    appendWiersz(&lista_wierszy, 1.25, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);// wieksza
-    appendWiersz(&lista_wierszy, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-    appendWiersz(&lista_wierszy, 1.12, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);// najmniejsza
-    appendWiersz(&lista_wierszy, 1.3, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-    appendWiersz(&lista_wierszy, 1.11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    appendWiersz(&lista_wierszy, 1.1, 67, 23, 24, 12, 65, 34, 23, 2, 76, 23, 1);
+    appendWiersz(&lista_wierszy, 1.25, 90, 24, 34, 43, 34, 23, -1, 34, 65, 54, 23);// wieksza
+    appendWiersz(&lista_wierszy, 1.5, 23, 34, 56, 26, 26, 34, 14, 54, 61, 24, 34);
+    appendWiersz(&lista_wierszy, 1.12, 8, 56, 65, 34, 34, 34, 45, 75, 75, 65, 43);// najmniejsza
+    appendWiersz(&lista_wierszy, 1.3, 10, 86, 723, -23, 34, 23, 25, 71, 734, 82, 23);
+    appendWiersz(&lista_wierszy, 1.11, 83, 25, 90, 73, 71, 26, 234, 45, 75, 75, 234);
 
     WierszNode *przeszukane3 = findByMBar(&lista_wierszy, 1.2);
 
@@ -153,7 +191,7 @@ int main(int argc, char **argv) {
     else {
         ParaWierszy *paraWierszy = znajdzNajblizszaPare(&lista_wierszy, 1.2);
         printf("mniejsza: %f\n", paraWierszy->mniejszy->m_grawitacyjna);
-        printf("wieksza: %f\n",  paraWierszy->wiekszy->m_grawitacyjna);
+        printf("wieksza: %f\n", paraWierszy->wiekszy->m_grawitacyjna);
     }
 
     destroyList(&lista_wierszy);
