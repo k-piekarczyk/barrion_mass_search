d2bbf2aed7d3dfc402649e1b7bb6b835360c9138
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2019-01-05 01:50:03 +0100

    finito

--- main.c
+++ main.c
@@ -5,11 +5,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #define BUFOR_ROZNICY 99999999
+#define BUFOR_LINI 256
 
 /** Definicja listy wierszy */
 typedef struct _wiersz_node {
+    int generated;
+
     double m_barionowa;
     double m_grawitacyjna;
     double m_pedu;
@@ -33,6 +37,7 @@ appendWiersz(WierszNode **head_ref, double m_barionowa, double m_grawitacyjna, d
     WierszNode *current_node = *head_ref;
 
     wyliczony->next = NULL;
+    wyliczony->generated = 0;
     wyliczony->m_barionowa = m_barionowa;
     wyliczony->m_grawitacyjna = m_grawitacyjna;
     wyliczony->m_pedu = m_pedu;
@@ -114,10 +119,10 @@ ParaWierszy *znajdzNajblizszaPare(WierszNode **head_ref, double m_bar_szukana) {
     }
 
     if (mniejsza == NULL) {
-        printf("Nie znaleziono wiersza z mniejsza masa barionowa niz: %f\n", m_bar_szukana);
+        fprintf(stderr, "Nie znaleziono wiersza z mniejsza masa barionowa niz: %f\n", m_bar_szukana);
         exit(EXIT_FAILURE);
     } else if (wieksza == NULL) {
-        printf("Nie znaleziono wiersza z wieksza masa barionowa niz: %f\n", m_bar_szukana);
+        fprintf(stderr, "Nie znaleziono wiersza z wieksza masa barionowa niz: %f\n", m_bar_szukana);
         exit(EXIT_FAILURE);
     }
 
@@ -129,18 +134,21 @@ ParaWierszy *znajdzNajblizszaPare(WierszNode **head_ref, double m_bar_szukana) {
     return paraWierszy;
 }
 
-double liniowaZaleznosc(double min_y, double max_y, double min_x, double max_x) {
-    double a, a1, a2, b, b1;
+double liniowaZaleznosc(double desirable_y, double min_y, double max_y, double min_x, double max_x) {
+
+    if (min_x == max_x) return min_x;
+
+    double a = 0, a1 = 0, a2 = 0, b = 0, b1 = 0, solution = 0;
     a1 = (max_y - min_y);
 
-    // wyliczanie masy grawitacyjnej
     a2 = (max_x - min_x);
     a = a1 / a2;
 
     b1 = ((min_y * max_x) - (max_y * min_x));
     b = b1 / a2;
 
-    return (min_y - b) / a;
+    solution = (desirable_y - b) / a;
+    return solution;
 }
 
 WierszNode *wyliczWierszLiniowo(ParaWierszy *paraWierszy) {
@@ -149,51 +157,154 @@ WierszNode *wyliczWierszLiniowo(ParaWierszy *paraWierszy) {
     WierszNode *wiekszy = paraWierszy->wiekszy;
 
     wyliczony->next = NULL;
+    wyliczony->generated = 1;
     wyliczony->m_barionowa = paraWierszy->m_bar_szukana;
-    wyliczony->m_grawitacyjna = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->m_grawitacyjna, wiekszy->m_grawitacyjna);
-    wyliczony->m_pedu = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->m_pedu, wiekszy->m_pedu);
-    wyliczony->R = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->R, wiekszy->R);
-    wyliczony->omega = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->omega, wiekszy->omega);
-    wyliczony->P = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->P, wiekszy->P);
-    wyliczony->Ro_B = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->Ro_B, wiekszy->Ro_B);
-    wyliczony->N_B = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->N_B, wiekszy->N_B);
-    wyliczony->entalpia = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->entalpia, wiekszy->entalpia);
-    wyliczony->r_eq = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->r_eq, wiekszy->r_eq);
-    wyliczony->s_r = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->s_r, wiekszy->s_r);
-    wyliczony->s_e = liniowaZaleznosc(mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->s_e, wiekszy->s_e);
+    wyliczony->m_grawitacyjna = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                                 mniejszy->m_grawitacyjna, wiekszy->m_grawitacyjna);
+    wyliczony->m_pedu = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                         mniejszy->m_pedu, wiekszy->m_pedu);
+    wyliczony->R = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->R,
+                                    wiekszy->R);
+    wyliczony->omega = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                        mniejszy->omega, wiekszy->omega);
+    wyliczony->P = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa, mniejszy->P,
+                                    wiekszy->P);
+    wyliczony->Ro_B = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                       mniejszy->Ro_B, wiekszy->Ro_B);
+    wyliczony->N_B = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                      mniejszy->N_B, wiekszy->N_B);
+    wyliczony->entalpia = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                           mniejszy->entalpia, wiekszy->entalpia);
+    wyliczony->r_eq = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                       mniejszy->r_eq, wiekszy->r_eq);
+    wyliczony->s_r = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                      mniejszy->s_r, wiekszy->s_r);
+    wyliczony->s_e = liniowaZaleznosc(wyliczony->m_barionowa, mniejszy->m_barionowa, wiekszy->m_barionowa,
+                                      mniejszy->s_e, wiekszy->s_e);
 
     return wyliczony;
 }
 
+WierszNode *findEquivalent(WierszNode **head_ref, double m_bar_szukana) {
+    ParaWierszy *paraWierszy = znajdzNajblizszaPare(head_ref, m_bar_szukana);
+    return wyliczWierszLiniowo(paraWierszy);
+}
 
-int main(int argc, char **argv) {
-    int m_barionowa, m_grawitacja;
-//
-//    if (argc < 2) {
-//        printf("Masa  barionowa:");
-//        scanf("%d", &m_barionowa);
-//    } else {
-//        m_barionowa = atoi(argv[1]);
-//    }
+WierszNode *findAnswer(WierszNode **head_ref, double m_bar_szukana) {
+    WierszNode *ans = findByMBar(head_ref, m_bar_szukana);
+    if (ans != NULL) return ans;
+    return findEquivalent(head_ref, m_bar_szukana);
+}
 
-    WierszNode *lista_wierszy = NULL;
+/** Wczytywanie pliku */
+void parseLine(const char *src, WierszNode **head_ref) {
+    char *bufor[BUFOR_LINI];
+    char *line = malloc(strlen(src) + 1);
+    strcpy(line, src);
 
-    appendWiersz(&lista_wierszy, 1.1, 67, 23, 24, 12, 65, 34, 23, 2, 76, 23, 1);
-    appendWiersz(&lista_wierszy, 1.25, 90, 24, 34, 43, 34, 23, -1, 34, 65, 54, 23);// wieksza
-    appendWiersz(&lista_wierszy, 1.5, 23, 34, 56, 26, 26, 34, 14, 54, 61, 24, 34);
-    appendWiersz(&lista_wierszy, 1.12, 8, 56, 65, 34, 34, 34, 45, 75, 75, 65, 43);// najmniejsza
-    appendWiersz(&lista_wierszy, 1.3, 10, 86, 723, -23, 34, 23, 25, 71, 734, 82, 23);
-    appendWiersz(&lista_wierszy, 1.11, 83, 25, 90, 73, 71, 26, 234, 45, 75, 75, 234);
+    double m_barionowa;
+    double m_grawitacyjna;
+    double m_pedu;
+    double R;               // promien chuja
+    double omega;           // predkosc katowa
+    double P;               // okres rotacji
+    double Ro_B;            // centralna gestosc energii
+    double N_B;             // gestosc barionowa
+    double entalpia;
+    double r_eq;            // promien przy rowniku
+    double s_r;             // stosunek promieni
+    double s_e;             // stosunek energii
 
-    WierszNode *przeszukane3 = findByMBar(&lista_wierszy, 1.2);
+    char *ptr = strtok(line, ",");
+    m_barionowa = atof(ptr);
 
-    if (przeszukane3 != NULL) printf("%f\n", przeszukane3->m_grawitacyjna);
-    else {
-        ParaWierszy *paraWierszy = znajdzNajblizszaPare(&lista_wierszy, 1.2);
-        printf("mniejsza: %f\n", paraWierszy->mniejszy->m_grawitacyjna);
-        printf("wieksza: %f\n", paraWierszy->wiekszy->m_grawitacyjna);
+    ptr = strtok(NULL, ",");
+    m_grawitacyjna = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    m_pedu = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    R = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    omega = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    P = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    Ro_B = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    N_B = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    entalpia = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    r_eq = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    s_r = atof(ptr);
+
+    ptr = strtok(NULL, ",");
+    s_e = atof(ptr);
+
+    appendWiersz(head_ref, m_barionowa, m_grawitacyjna, m_pedu, R, omega, P, Ro_B, N_B, entalpia, r_eq, s_r, s_e);
+}
+
+WierszNode *loadData(const char *fileName) {
+    FILE *fp;
+    char line[BUFOR_LINI];
+    WierszNode *dataList = NULL;
+
+    fp = fopen(fileName, "r");
+    if (fp == NULL) {
+        printf("No file '%s'.\n", fileName);
+        exit(EXIT_FAILURE);
     }
 
+    while (fgets(line, sizeof line, fp) != NULL) {
+        strtok(line, "\n");
+        parseLine(line, &dataList);
+    }
+    fclose(fp);
+    return dataList;
+}
+
+/** Wypisywanie poszukiwanych danych */
+void printCalculatedData(WierszNode *calcData) {
+    printf("%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f\n", calcData->m_barionowa,
+           calcData->m_grawitacyjna, calcData->m_pedu, calcData->R, calcData->omega, calcData->P, calcData->Ro_B,
+           calcData->N_B, calcData->entalpia, calcData->r_eq, calcData->s_r, calcData->s_e);
+}
+
+int main(int argc, char **argv) {
+    double m_barionowa;
+    char *fileName;
+
+    if (argc < 2) {
+        printf("Nie podales nazwy pliku.\n");
+        exit(EXIT_FAILURE);
+
+    } else {
+        fileName = argv[1];
+    }
+
+    WierszNode *lista_wierszy = loadData(fileName);
+
+    if (argc < 3) {
+        printf("Podaj mase barionowa: ");
+        scanf("%f", &m_barionowa);
+    } else {
+        m_barionowa = atof(argv[2]);
+    }
+
+    WierszNode *answer = findAnswer(&lista_wierszy, m_barionowa);
+    printCalculatedData(answer);
+
+    if (answer->generated == 1) free(answer);
     destroyList(&lista_wierszy);
     return 0;
 }
